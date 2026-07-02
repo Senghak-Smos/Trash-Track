f9a2e32f0b86898e7904689a5b6a67d95ffaba0f
Nang Senghak <haksmos83@gmail.com>
2026-07-02 16:12:44 +0700

    Update KK3

--- Eng_kh_upload.js
+++ Eng_kh_upload.js
@@ -12,3 +12,28 @@ langSelect_upload.addEventListener("change", function () {
     window.location.href = this.value;
   }
 });
+
+// ============== for upload image =================
+const imageInput = document.getElementById("imageinput");
+const imagePreview = document.getElementById("imagepreview");
+
+const texttitle = document.querySelector(".container-title h4");
+const textsupport = document.querySelector(".container-title p");
+
+// លុប Event Listener លើការ click ចេញ ព្រោះយើងបានប្រើ <label for="imageinput"> នៅក្នុង HTML រួចហើយ
+
+imageInput.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const read = new FileReader();
+    read.addEventListener("load", function () {
+      // ប្ដូររូបភាព preview ទៅជារូបភាពដែលបានជ្រើសរើស
+      imagePreview.setAttribute("src", this.result);
+
+      // លាក់អក្សរផ្សេងៗពេលមានរូបភាព
+      if (texttitle) texttitle.style.display = "none";
+      if (textsupport) textsupport.style.display = "none";
+    });
+    read.readAsDataURL(file);
+  }
+});
